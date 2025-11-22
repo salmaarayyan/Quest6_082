edfff7710f1733e9d94ac3984efab4d9d67521e9
salmaarayyan <salmaarayyan@gmail.com>
2025-11-22 14:52:39 +0700

    tambah parameter viewModel & navController

--- app/src/main/java/com/example/quest6_082/Navigasi.kt
+++ app/src/main/java/com/example/quest6_082/Navigasi.kt
@@ -1,6 +1,9 @@
 package com.example.quest6_082
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 
 
 enum class Navigasi {
@@ -9,4 +12,8 @@ enum class Navigasi {
 }
 
 @Composable
-fun SiswaApp(){}
+fun SiswaApp(
+    modifier: Modifier,
+    viewModel: SiswaViewModel = viewModel(),
+    navController: NavController = rememberNavController()
+){}
