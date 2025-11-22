159b6b50b9492547506d103098c13fd225ee4e59
salmaarayyan <salmaarayyan@gmail.com>
2025-11-22 15:05:06 +0700

    menambahkan state nama, alamat, gender serta listData

--- app/src/main/java/com/example/quest6_082/view/Formulir.kt
+++ app/src/main/java/com/example/quest6_082/view/Formulir.kt
@@ -1,9 +1,21 @@
 package com.example.quest6_082.view
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 
 @Composable
 fun FormIsian(
     pilihanJK: List<String>,
-    OnSubmitBtnClick: (MutableList<String> -> Unit){
+    OnSubmitBtnClick: (MutableList<String> -> Unit
+){
+    var txtNama by rememberSaveable { mutableStateOf("") }
+    var txtAlamat by remember { mutableStateOf("") }
+    var txtGender by remember { mutableStateOf("") }
+    var listData: MutableList<String> = mutableListOf(
+        txtNama,
+        txtGender,
+        txtAlamat
+    )
 }
