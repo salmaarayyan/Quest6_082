ff3a021d679691e2fde0df0e830ce26e096a79b5
salmaarayyan <salmaarayyan@gmail.com>
2025-11-22 15:08:40 +0700

    menambahkan layout Column dengan padding dari isiRuang

--- app/src/main/java/com/example/quest6_082/view/Formulir.kt
+++ app/src/main/java/com/example/quest6_082/view/Formulir.kt
@@ -1,9 +1,18 @@
 package com.example.quest6_082.view
 
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.stringResource
+import com.example.quest6_082.R
 
 @Composable
 fun FormIsian(
@@ -26,5 +35,10 @@ fun FormIsian(
                 colors = TopAppBarDefaults.topAppBarColors
                     (colorResource(id = R.color.teal_700))
             ) }
-    ){}
+    ){ isiRuang ->
+        Column(modifier = Modifier.padding(isiRuang),
+            verticalArrangement = Arrangement.Top,
+            horizontalAlignment = Alignment.CenterHorizontally)
+
+    }
 }
