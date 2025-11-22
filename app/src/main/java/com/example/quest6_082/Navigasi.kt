2b0048a9b7a8f5af04ba2613638616a35b7cbdeb
salmaarayyan <salmaarayyan@gmail.com>
2025-11-22 14:57:00 +0700

    tambah navigasi Formulirku ke Detail

--- app/src/main/java/com/example/quest6_082/Navigasi.kt
+++ app/src/main/java/com/example/quest6_082/Navigasi.kt
@@ -1,5 +1,7 @@
 package com.example.quest6_082
 
+import androidx.compose.material3.NavigationBar
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
@@ -16,4 +18,25 @@ fun SiswaApp(
     modifier: Modifier,
     viewModel: SiswaViewModel = viewModel(),
     navController: NavController = rememberNavController()
-){}
+){
+    Scaffold { isiRuang->
+        val uiState = viewModel.statusUI.collectAsState()
+        NavHost(
+            navController = navController,
+            startDestination = Navigasi.Formulirku.name,
+
+            modifier = Modifier.padding(isiRuang)
+        ){
+            composable(route = Navigasi.Formulirku.name){
+                val konteks = LocalContext.current
+                FormIsian(
+                    pilihanJK = JenisK.map {id -> konteks.resources.getString(id)},
+                    onSubmitBtnClick = {
+                        viewModel.setSiswa(it)
+                        navController.navigate(Navigasi.Detail.name)
+                    }
+                )
+            }
+        }
+    }
+}
