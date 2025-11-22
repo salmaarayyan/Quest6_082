02d42a895dade5b80e3ad2045dbd8f0786667a95
salmaarayyan <salmaarayyan@gmail.com>
2025-11-22 15:31:01 +0700

    menambahkan SiswaViewModel untuk mengelola data Siswa

---
+++ app/src/main/java/com/example/quest6_082/viewmodel/SiswaViewModel.kt
@@ -0,0 +1,24 @@
+package com.example.quest6_082.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.quest6_082.model.Siswa
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class SiswaViewModel : ViewModel() {
+    private val _statusUI = MutableStateFlow(Siswa())
+
+    val statusUI: StateFlow<Siswa> = _statusUI.asStateFlow()
+
+    fun setSiswa(ls: MutableList<String>) {
+        _statusUI.update { statusSaatIni ->
+            statusSaatIni.copy(
+                nama = ls[0],
+                gender = ls[1],
+                alamat = ls[2]
+            )
+        }
+    }
+}
