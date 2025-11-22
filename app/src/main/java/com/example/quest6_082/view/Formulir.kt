eca31521291d9e3dad317acf8d44aa8840dd59e5
salmaarayyan <salmaarayyan@gmail.com>
2025-11-22 15:22:24 +0700

    revisi kode di Formulir.kt

--- app/src/main/java/com/example/quest6_082/view/Formulir.kt
+++ app/src/main/java/com/example/quest6_082/view/Formulir.kt
@@ -19,9 +19,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -33,7 +35,7 @@ import com.example.quest6_082.R
 @Composable
 fun FormIsian(
     pilihanJK: List<String>,
-    OnSubmitBtnClick: (MutableList<String> -> Unit
+    OnSubmitBtnClick: (MutableList<String>) -> Unit
 ){
     var txtNama by rememberSaveable { mutableStateOf("") }
     var txtAlamat by remember { mutableStateOf("") }
@@ -43,6 +45,7 @@ fun FormIsian(
         txtGender,
         txtAlamat
     )
+
     Scaffold (modifier = Modifier,
         {
             TopAppBar(
@@ -114,6 +117,5 @@ fun FormIsian(
                 Text(stringResource(id = R.string.submit))
             }
         }
-
     }
 }
